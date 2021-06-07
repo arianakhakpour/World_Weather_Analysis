a82bfd010b405b6b5d0151307783d4a0208ed56f
Ariana Khakpour <82549848+arianakhakpour@users.noreply.github.com>
2021-06-06 23:23:00 -0400

    Add files via upload

---
+++ config4.py
@@ -0,0 +1,4 @@
+weather_api_key = '2c761883f32929dd5b79bc152646f4e8'
+g_key = 'AIzaSyB3X8QN2eSkpyQHSFJB0p3DTe5a6gd-zm8'
+p_key = 'AIzaSyB4yMmed-BWaHLBLYMltF-L0V949P4THjs'
+dir_key = 'AIzaSyCSxvl1nkyM2EVshMlm0MwDhod5o0HEATs'
